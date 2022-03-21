f00b2c32d2c9944ce26df6390902f919c254922d
Shibumishra <sibumishra487@gmail.com>
2022-03-21 23:14:32 +0530

    Updates

---
+++ precache-manifest.fb233ab957abda3204d79b41af572639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16863a3ca38105d61e4f72f808f8e74",
+    "url": "/comment-post/index.html"
+  },
+  {
+    "revision": "20e352b583c76a63ac64",
+    "url": "/comment-post/static/css/main.39a76435.chunk.css"
+  },
+  {
+    "revision": "4d212ff950d3e4eb28f5",
+    "url": "/comment-post/static/js/2.6307d724.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/comment-post/static/js/2.6307d724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e352b583c76a63ac64",
+    "url": "/comment-post/static/js/main.256874c8.chunk.js"
+  },
+  {
+    "revision": "6f27be8cc1f81d766489",
+    "url": "/comment-post/static/js/runtime-main.6b03152e.js"
+  }
+]);
